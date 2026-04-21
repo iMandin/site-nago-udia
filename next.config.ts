5768f5c82235594741c5aa9f0318fd77cf91e6bf
iMandin <amandavieira0429@gmail.com>
2026-04-21 15:02:25 -0300

    fix next.config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        aggregateTimeout: 200,
-        poll: 1000,
-        ignored: /node_modules/,
-      };
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
